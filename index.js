3a061bf5439e6e689036bc0030ec35c363f2e111
Smbat-G <167191193+Smbat-G@users.noreply.github.com>
2024-04-22 16:55:18 +0400

    test2

--- index.js
+++ index.js
@@ -10,13 +10,11 @@
 
 // let avg2 = 0
 // let z = y.forEach(function(item, index){
-//     avg2 += item / y.length  
+//     avg2 += item / y.length
 // })
 
 // sum = avg * avg2
 // console.log(sum);
-
-
 
 // 262
 
@@ -34,8 +32,6 @@
 //     sum = avg + avg2
 //     console.log(sum);
 
-
-
 // 263
 // let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
 // let y = [0, 3, -5, -1, 5, 8, 7, 9, 4]
@@ -50,7 +46,6 @@
 // }
 // })
 
-
 // let a = y.forEach(function(item, index){
 //     if(item > 0){
 //         count2 ++
@@ -59,8 +54,6 @@
 
 //     sum = count + count2
 //     console.log(sum);
-
-
 
 // 264
 // let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
@@ -76,7 +69,6 @@
 // }
 // })
 
-
 // let a = y.forEach(function(item, index){
 //     if(item < 0){
 //         count2 ++
@@ -86,8 +78,7 @@
 //     sum = count + count2
 //     console.log(sum);
 
-
-// 265 pahanj ???
+// 265
 // let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
 // let y = [-2, 3, -5, -1, 5, 8, 7, 9, 4]
 // let count = 0
@@ -98,10 +89,13 @@
 //    count += item
 // })
 // let a = y.forEach(function(item, index){
+//     if(item > 0){
 //     count2 *= item
+//     }
 //  })
 
-
+//  sum = count / count2
+//  console.log(sum);
 
 // 266
 
@@ -126,26 +120,218 @@
 //  sum = count - count2
 //  console.log(sum);
 
-
 // 267
 
-let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
-let y = [-2, 3, -5, -1, 5, 8, 7, 9, 4]
-let count = 0
-let sum = 0
-let count2 = 0
+// let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
+// let y = [-2, 3, -5, -1, 5, 8, 7, 9, 4]
+// let count = 0
+// let sum = 0
+// let count2 = 0
 
-let z = x.forEach(function(item, index){
-    if(item  % 7 === 0){
-   count ++
-    }
-})
-console.log(count);
-let a = y.forEach(function(item, index){
-    if(item % 7 === 0){
-        count2 ++
-         }
- })
-console.log(count2);
- sum = count - count2
- console.log(sum);
+// let z = x.forEach(function(item, index){
+//     if(item  % 7 === 0){
+//    count += item
+//     }
+// })
+// console.log(count);
+// let a = y.forEach(function(item, index){
+//     if(item % 7 === 0){
+//         count2 += item
+//          }
+//  })
+// console.log(count2);
+//  sum = count + count2
+//  console.log(sum);
+
+// 268
+
+// let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
+// let y = [-2, 3, -5, -1, 5, 8, 7, 9, 4]
+// let count = 0
+// let sum = 0
+// let count2 = 0
+
+// let z = x.forEach(function(item, index){
+//     if(item  % 7 === 0){
+//    count ++
+//     }
+// })
+// console.log(count);
+// let a = y.forEach(function(item, index){
+//     if(item % 7 === 0){
+//         count2 ++
+//          }
+//  })
+// console.log(count2);
+//  sum = count + count2
+//  console.log(sum);
+
+// 269
+
+// let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
+// let y = [-2, 3, -5, -1, 5, 8, 7, 9, 4]
+// let count = 0
+// let sum = 0
+// let count2 = 0
+
+// let z = x.forEach(function(item, index){
+//     if(item  % 2 === 0){
+//    count ++
+//     }
+// })
+// console.log(count);
+// let a = y.forEach(function(item, index){
+//     if(item % 2 !== 0){
+//         count2 ++
+//          }
+//  })
+// console.log(count2);
+//  sum = count + count2
+//  console.log(sum);
+
+// 270
+
+// let x = [1, -7, -1, 2, 5, 6, 8, 7, 9]
+// let y = [-2, 3, -5, -1, 5, 8, 7, 9, 4]
+// let count = 0
+// let sum = 0
+// let count2 = 0
+
+// let z = x.forEach(function(item, index){
+
+//    count += item * item
+
+// })
+// console.log(count);
+// let a = y.forEach(function(item, index){
+
+//         count2 += item * item
+
+//  })
+// console.log(count2);
+//  sum = count + count2
+//  console.log(sum);
+
+// 271
+
+// let list = ['a', 'v', 6, 'f', 'a', 'h', 't', 'p']
+// let count = 0
+// let result = list.forEach(function(item, index){
+//     if(item === 'a'){
+//         count++
+//     }
+// })
+// console.log(count);
+
+// 272
+
+// let list = ['a', 'v', 6, 'f', 'a', 'h', 's', 's', 'p']
+// let count = 0
+// let t = false
+
+// for(let i = 0; i < list.length; i++){
+//     if(list[i] === 'b'){
+//         count++
+//     }
+//     if(list.length / 2 === count){
+//         t = true
+//     }else{
+//         t = false
+//     }
+// }
+// console.log(t);
+
+// 273
+
+// let list = ['a', 'v', 6, 'f', 'a', 'h', 's', 's', 'p']
+// let count = 0
+// for(let i = 0; i < list.length; i++){
+//     if(list[i] === 's'){
+//         count += i
+//     }
+// }
+// console.log(count);
+
+// 274
+
+// let list = ['a', 'v', 6, 'f', 'a', 'h', 's', 's', 'p']
+// let count = 0
+// for(let i = 0; i < list.length; i++){
+//     if(typeof list[i] === 'string' && list[i] > 'h'){
+//         count += i / i
+//     }
+// }
+// console.log(count);
+
+//275
+
+// let list = ['a', 'v', 6, 'f', 'a', 'h', 's','k', 's', 'p']
+// let count = 0
+// for(let i = 0; i < list.length; i++){
+//     if(typeof list[i] === 'string' && list[i] < 'k'){
+//         count++
+//     }
+// }
+// console.log(count);
+
+// 276
+
+// let list = ["a", "v", 6, "r", "p", "h", "s", "k", "s", "p"];
+// let count = 0;
+// let t = false;
+// for (let i = 0; i < list.length; i++) {
+//   if (typeof list[i] === "string" && list[i] === "r") {
+//     t = true;
+//     break;
+//   }
+// }
+// console.log(t);
+
+
+
+// 277 ???
+
+// let list = ["a", "v", 6, "r", "p", "h", "s", "k", "s", "p"];
+
+// let result = list.filter(function(item, index){
+//     return typeof item === "string" && item > "k"
+// })
+// console.log(result);
+
+
+
+// 278
+
+// let list = ["a", "v", 6, "r", "p", "h", "s", "k", "s", "p"];
+// console.log(list);
+// let result = list.filter(function(item, index){
+//     return typeof item === "string" && index % 2 !== 0
+// })
+// console.log(result);
+
+
+
+// 279 ???
+
+// let list = ["a", "v", 6, "r", "p", "h", "s", "k", "s", "p"];
+// console.log(list);
+// let result = list.filter(function(item, index){
+//     return typeof item === "string" && item > 'k'
+// })
+// console.log(result);
+
+
+
+// 280 ???? inchi chi verjis avelanum f bayc hamapatasxanum e pahanjin lucum@
+
+// let list = ["a", "v", "f", "r", "p", "h", "f", "k", "s", "p"];
+// console.log(list);
+// let newList = [];
+// for (let i = 0; i < list.length; i++) {
+//     newList.push(list[i])
+//     if (typeof list[i] === "string" && list[i] === 'f') {
+//         newList.push('f')
+//     }
+// }
+// console.log(newList);
+
